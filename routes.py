af9898db4999a7ad5524220d5cf9cd901270dbe2
hershalb <sunil@calvisitor-10-105-151-114.calvisitor.1918.berkeley.edu>
2015-12-05 00:13:35 -0800

    Working Signup route

--- routes.py
+++ routes.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import db
+from forms import SignupForm
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/information'
+db.init_app(app)
+
+app.secret_key = "development-key"
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-	return render_template("index.html")
+	form = SignupForm()
+
+	if request.method == "POST":
+		return "Success!"
+	elif request.method == "GET":
+		return render_template("index.html", form = form)
+
+# @app.route("/signup")
+# def signup():
+# 	form = SignupForm()
+# 	return render_template("index.html", form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
